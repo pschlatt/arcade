7ba9cc4b562fa78829e12bbbf58ac0dab8978f0b
Paul Schlattmann <paulschlattmann@Pauls-MacBook-Pro.local>
2020-08-24 10:14:19 -0700

    02/26/21 16:05:40

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-/* GET home page. */
-/* GET home page. */
-/* GET home page. */
+/* home Get*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
