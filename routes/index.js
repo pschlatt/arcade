4fd5727274b8d32c1cfef362fc0c324688198d1d
Paul Schlattmann <paulschlattmann@Pauls-MacBook-Pro.local>
2021-02-26 16:07:28 -0700

    02/26/21 16:05:40

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var router = express.Router();
 /* GET home page. */
 /* GET home page. */
 /* GET home page. */
+/* GET home page. */
+/* GET home page. */
+/* GET home page. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
